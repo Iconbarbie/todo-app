8802c96637eb949b9acdb560316cb167545474f7
Iconbarbie <happiness.oiro@gmail.com>
2022-11-14 09:35:03 +0300

    todo-app

---
+++ todo-app.js
@@ -0,0 +1,6 @@
+console.log("playing basketball")
+
+//add new element
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "happiness likes swimming";
+document.querySelector("body").appendChild(newParagraph);
